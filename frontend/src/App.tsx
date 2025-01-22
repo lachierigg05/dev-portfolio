ab29d0de01b5ef900efc16adf1ab9400b99694ca
Lachlan Rigg <111475731+lachierigg05@users.noreply.github.com>
2025-01-23 08:23:45 +1100

    Socials and communication logos added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import DevProfileCard from "./components/DevProfileCard.tsx";
+import '../index.css'
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      const flashlight = document.getElementById('flashlight');
+      if (flashlight) {
+        flashlight.style.left = `${event.clientX}px`;
+        flashlight.style.top = `${event.clientY}px`;
+      }
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
-    <div className="relative min-h-screen w-full bg-slate-950">
-      <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-      <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+    <div>
+      <DevProfileCard />
+      <div id="flashlight" className="flashlight"></div>
     </div>
   );
 };
